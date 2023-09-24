68fd42d60409c38b4cb3b4e16b46b7879f7c4605
everulicess <ulices.vaca12@gmail.com>
2023-09-24 22:53:21 +0200

    trigger animations in hands
    Used hand models and animations to test how the input works

---
+++ Assets/A.S.S.PROJECT/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+
+    [SerializeField]
+    Animator handAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue =  pinchAnimationAction.action.ReadValue<float>();
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+
+        handAnimator.SetFloat("Trigger", triggerValue);
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+}
